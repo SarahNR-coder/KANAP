2f7f558a08056e1fecd15963f0131d47aead9297
Sarah Richou <sarah.richou@hotmail.fr>
2022-01-01 12:02:11 +0100

    commentaires

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,3 +1,32 @@
+//fonctions de récupération de données-------------
+
+//récupération des données de catalogue de canapés (API)
+const retrieveData = () =>fetch("http://localhost:3000/api/products")
+.then(res =>{
+    if (res.ok){
+        return res.json();
+    }
+})
+.then (data => {
+    console.log(data);
+    return data;})
+.catch (err => console.log('erreur suivante:'+ err))
+
+//récupération dans le tableau-panier (localStorage)des produits commandés d'un certain id et parmi ceux qui ont cet id, d'une certaine couleur; Un article pour une combinaison id-couleur
+const setPurchase=(LineValue0)=> {
+    var finalStorageArr = new Array([""]);
+    finalStorageArr[0] = LineValue0;      
+    for(let i=1; i<localStorage.length; i++){ 
+        var LineValue= localStorage.getItem(localStorage.key(i));            
+        finalStorageArr[i]=LineValue;
+    }
+    return finalStorageArr;
+}
+
+
+
+//fonctions de calcul de prix string à partir de prix nombres et l'inverse
+
 var priceKanapCalc =(priceKanap0)=>{
     var priceKanap ="";
     var priceKanap1 ="";
@@ -46,30 +75,87 @@ var priceKanapCalc =(priceKanap0)=>{
     return priceKanap;
 }
 
+//Fonctions création de contenu---------------------
 
-const retrieveData = () =>fetch("http://localhost:3000/api/products")
-.then(res =>{
-    if (res.ok){
-        return res.json();
-    }
-})
-.then (data => {
-    console.log(data);
-    return data;})
-.catch (err => console.log('erreur suivante:'+ err))
+//création de l'image de la vignette produit-panier à partir d'une variable qui est un block div itemImage où l'on insére l'url de l'image et son texte alternatif, block image que l'on retourne;
+var addImageToItemCart =(itemImage, imageUrlKanap, altTxtKanap)=>
+{
 
+    const itemImageShown = document.createElement('img');
+    itemImageShown.setAttribute('src', imageUrlKanap);
+    itemImageShown.setAttribute('alt', altTxtKanap);
 
-const setPurchase=(LineValue0)=> {
-    var finalStorageArr = new Array([""]);
-    finalStorageArr[0] = LineValue0;      
-    for(let i=1; i<localStorage.length; i++){ 
-        var LineValue= localStorage.getItem(localStorage.key(i));            
-        finalStorageArr[i]=LineValue;
-    }
-    return finalStorageArr;
+    itemImage.appendChild(itemImageShown);
+
+    return itemImage;
 }
+//création du contenu de la vignette produit-panier à partir d'une variable qui est un block div itemContent, où l'on insère la couleur, le nom, et le prix du produit-panier dans un block div description et sa quantité dans un élément input (quantité changeable), block content que l'on retourne;
 
-const createArticle=(data)=>{
+var addContentToItemCart =(itemContent, color, quantity, nameKanap, priceKanap) =>
+{
+    //#.1
+    const contentDescription =document.createElement('div');
+    contentDescription.classList.add('cart__item__content__description');
+    //#.1
+    const contentSettings =document.createElement('div');
+    contentSettings.classList.add('cart__item__content__settings');
+
+    //#
+    itemContent.appendChild(contentDescription);//#.1
+    itemContent.appendChild(contentSettings);//#.2
+
+    //#.1.1
+    const DescriptionTitle = document.createElement('h2');
+    DescriptionTitle.textContent = nameKanap;
+    //#.1.2 
+    const DescriptionFirstP = document.createElement('p');
+    DescriptionFirstP.textContent = color;
+    //#.1.3
+    const DescriptionSecondP = document.createElement('p');
+    DescriptionSecondP.textContent = priceKanap;
+
+    //#.1
+    contentDescription.appendChild(DescriptionTitle)//#.1.1
+    contentDescription.appendChild(DescriptionFirstP);//#.1.2
+    contentDescription.appendChild(DescriptionSecondP);//#.1.3
+
+    //#.2.1
+    const settingsQuantity = document.createElement('div');
+    settingsQuantity.classList.add('cart__item__content__settings__quantity');
+    //#.2.2
+    const settingsDelete = document.createElement('div');
+    settingsDelete.classList.add('cart__item__content__settings__delete');
+
+    //#.2
+    contentSettings.appendChild(settingsQuantity);//#.2.1
+    contentSettings.appendChild(settingsDelete);//#.2.2
+
+    //#.2.1.1
+    const QuantityP = document.createElement('p');
+    QuantityP.textContent ='Qté :';
+    //#.2.1.2
+    const QuantityInput = document.createElement('input');
+    QuantityInput.setAttribute('type','number');
+    QuantityInput.setAttribute('name', 'ItemQuantity');
+    QuantityInput.setAttribute('min', '1');
+    QuantityInput.setAttribute('max','100');
+    QuantityInput.setAttribute('value', quantity);
+
+    //#.2.1
+    settingsQuantity.appendChild(QuantityP);//#.2.1.1
+    settingsQuantity.appendChild(QuantityInput);//#.2.1.2
+
+    //#.2.2.1
+    const DeleteP = document.createElement('p');
+    DeleteP.classList.add('deleteItem');
+    DeleteP.textContent='Supprimer';
+
+    //#.2.2
+    settingsDelete.appendChild(DeleteP);//#.2.2.1
+
+    return itemContent;
+}
+var createArticle=(data)=>{
     const itemsCart = document.getElementById('cart__items');
     const LineValue0 = localStorage.getItem(localStorage.key(0));
     const finalStorageArr = setPurchase(LineValue0);
@@ -114,15 +200,15 @@ const createArticle=(data)=>{
         itemCart.setAttribute('data-color',color); 
     
     //*Boucle for : à chaque ligne du LocalStorage (panier) on extrait l'id, la couleur et la quantité;
-    //**recherche de l'élement du panier dans le catalogue (data) grâce à l'id
+    //****recherche de l'élement du panier dans le catalogue (data) grâce à l'id
     //***pour chaque produit-panier que l'on ajoute on augmente la quantité globale de la quantité d'occurences de ce produit-panier 
-    //je crée une vignette itemCart 'article.cart__item' pour chaque produit-panier correspondant chacunes à une ligne du tableau-panier (localStorage)
+    //**je crée une vignette itemCart 'article.cart__item' pour chaque produit-panier correspondant chacunes à une ligne du tableau-panier (localStorage)
 
     totalQuantity0 += quantity0;
     //***                
     
         var j=0;
-        do{//**
+        do{//****
             if(data[j]._id == id){
                 var idKanap = "";
                 priceKanap0 = data[j].price;
@@ -159,10 +245,10 @@ const createArticle=(data)=>{
         }while(j<data.length); 
 
         itemsCart.appendChild(itemCart);
-        //à chaque ligne de tableau-panier (localStorage), correspondant à un Kanapé donné avec une couleur donnée, correspond une vignette produit itemCart 'article.item__cart', sous la 'section.items__cart' nommée itemsCart;
-        //on renvoie la section remplie par les vignettes produit
-
         return itemsCart;
+
+        //à chaque ligne de tableau-panier (localStorage), correspondant à un Kanapé donné avec une couleur donnée, correspond une vignette produit itemCart 'article.item__cart', que l'on ajoute (appendChild())sous la 'section.items__cart' nommée itemsCart;
+        //on renvoie la section remplie par les vignettes produit qui est une variable car une fois établie elle peut changer puisque l'on peut supprimer des vignettes itemCart;
     }
 
     totalQuantity= totalQuantity0.toString();
@@ -173,95 +259,14 @@ const createArticle=(data)=>{
     const totalPriceHTML = document.getElementById('totalPrice');
     totalPriceHTML.textContent= totalPrice;
 }
-// la fonction createArticle à partir de la constante data (attendue) et de la constante finalStorageArr, en utilisant la fonction itemCart, ajoute en tant qu'éléments enfants des variables  HTMLElements <articles> nommées cart à la constante HTMLElement itemsCart;
 
-//Rôle de la fonction itemCart qui renvoie une variable:
 
-//ses paramètres sont les variables idKanap, color, quantity, nameKanap, priceKanap, imageUrlKanap et altTxtKanap;
-// on les appellent ici variablesKanap;
 
-// on prend les constantes data(après attente) et finalStorageArr;
-//dans la fonctions on en retire des variablesKanap
-//on fait passer ces variablesKanap dans la fonction itemCart
-//on en retire la variable HTMElement <article> cart
 
-const addImageToItemCart =(itemImage, imageUrlKanap, altTxtKanap)=>
-{
 
-    const itemImageShown = document.createElement('img');
-    itemImageShown.setAttribute('src', imageUrlKanap);
-    itemImageShown.setAttribute('alt', altTxtKanap);
 
-    //#1.1
-    itemImage.appendChild(itemImageShown);
 
-    return itemImage;
-}
 
-const addContentToItemCart =(itemContent, color, quantity, nameKanap, priceKanap) =>
-{
-    //#1.2.1
-    const contentDescription =document.createElement('div');
-    contentDescription.classList.add('cart__item__content__description');
-    //#1.2.2
-    const contentSettings =document.createElement('div');
-    contentSettings.classList.add('cart__item__content__settings');
-
-    //#1.2
-    itemContent.appendChild(contentDescription);//#1.2.1
-    itemContent.appendChild(contentSettings);//#1.2.2
-
-    //#1.2.1.1
-    const DescriptionTitle = document.createElement('h2');
-    DescriptionTitle.textContent = nameKanap;
-    //#1.2.1.2 
-    const DescriptionFirstP = document.createElement('p');
-    DescriptionFirstP.textContent = color;
-    //#1.2.1.3
-    const DescriptionSecondP = document.createElement('p');
-    DescriptionSecondP.textContent = priceKanap;
-
-    //#1.2.1
-    contentDescription.appendChild(DescriptionTitle)//   #1.2.1.1
-    contentDescription.appendChild(DescriptionFirstP);//#1.2.1.2
-    contentDescription.appendChild(DescriptionSecondP);// //#1.2.1.3
-
-    //#1.2.2.1
-    const settingsQuantity = document.createElement('div');
-    settingsQuantity.classList.add('cart__item__content__settings__quantity');
-    //#1.2.2.2
-    const settingsDelete = document.createElement('div');
-    settingsDelete.classList.add('cart__item__content__settings__delete');
-
-    //#1.2.2
-    contentSettings.appendChild(settingsQuantity);//#1.2.2.1
-    contentSettings.appendChild(settingsDelete);//#1.2.2.2
-
-    //#1.2.2.1.1
-    const QuantityP = document.createElement('p');
-    QuantityP.textContent ='Qté :';
-    //#1.2.2.1.2
-    const QuantityInput = document.createElement('input');
-    QuantityInput.setAttribute('type','number');
-    QuantityInput.setAttribute('name', 'ItemQuantity');
-    QuantityInput.setAttribute('min', '1');
-    QuantityInput.setAttribute('max','100');
-    QuantityInput.setAttribute('value', quantity);
-
-    //#1.2.2.1
-    settingsQuantity.appendChild(QuantityP);//#1.2.2.1.1
-    settingsQuantity.appendChild(QuantityInput);//#1.2.2.1.2
-
-    //#1.2.2.2.1
-    const DeleteP = document.createElement('p');
-    DeleteP.classList.add('deleteItem');
-    DeleteP.textContent='Supprimer';
-
-    //#1.2.2.2
-    settingsDelete.appendChild(DeleteP);//#1.2.2.2.1
-
-    return itemContent;
-}
 var totalPrice0Calc =(totalPrice)=>{
       
     var tPlength = totalPrice.length;
